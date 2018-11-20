2213038d59b9e800100d020674cc4e636503e21c
stephan <webmaster1@beyondjava.de>
2018-11-20 22:45:40 +0100

    test case for #9 (exception if the PDF is removed from the page before it is drawn)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title = 'ngx-extended-pdf-viewer';
   public pdf = 'assets/example.pdf';
+  public hidden = false;
 }
