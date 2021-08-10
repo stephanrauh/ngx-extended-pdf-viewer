30b8c4a2c0a549e1d6fa4d9467a213e371d11e06
stephan <webmaster1@beyondjava.de>
2021-08-10 22:13:44 +0200

    #843 optionally disable embedded JavaScript

--- projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
+++ projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
@@ -3,7 +3,7 @@ const isEdge = typeof navigator === 'undefined' || /Edge\/\d./i.test(navigator.u
 const needsES5 = typeof ReadableStream === 'undefined' || typeof Promise['allSettled'] === 'undefined';
 
 export let pdfjsVersion = '2.10.539';
-export let pdfjsBleedingEdgeVersion = '2.11.161';
+export let pdfjsBleedingEdgeVersion = '2.11.262';
 
 export function getVersionSuffix(folder: string): string {
   if (folder && folder.includes('bleeding-edge')) {
