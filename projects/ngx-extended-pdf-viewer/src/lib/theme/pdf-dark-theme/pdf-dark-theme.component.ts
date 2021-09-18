9c2c9c7e610d7ac6c442a780f1b692b73a8752ea
stephan <webmaster1@beyondjava.de>
2021-09-18 14:27:54 +0200

    added CSS rules recently added by pdf.js; updated Angular

--- projects/ngx-extended-pdf-viewer/src/lib/theme/pdf-dark-theme/pdf-dark-theme.component.ts
+++ projects/ngx-extended-pdf-viewer/src/lib/theme/pdf-dark-theme/pdf-dark-theme.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'pdf-dark-theme',
   templateUrl: './pdf-dark-theme.component.html',
-  styleUrls: ['./colors.scss', '../common/print.scss'],
+  styleUrls: ['./colors.scss', '../common/print.scss', '../common/xfa_layer_builder.css'],
   encapsulation: ViewEncapsulation.None,
 })
 export class PdfDarkThemeComponent {}
--- projects/ngx-extended-pdf-viewer/src/lib/theme/pdf-light-theme/pdf-light-theme.component.ts
+++ projects/ngx-extended-pdf-viewer/src/lib/theme/pdf-light-theme/pdf-light-theme.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'pdf-light-theme',
   templateUrl: './pdf-light-theme.component.html',
-  styleUrls: ['./colors.scss', '../common/print.scss'],
+  styleUrls: ['./colors.scss', '../common/print.scss', '../common/xfa_layer_builder.css'],
   encapsulation: ViewEncapsulation.None,
 })
 export class PdfLightThemeComponent {}
