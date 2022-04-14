5ff997f5ccd981bc63e29d49a396ab108420dabf
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2022-04-14 22:02:20 +0200

    #1300 if op-chaining-support.js is loaded twice for some reason, the PDF viewer prints an error message to the console, and possibly even falls back to the ES5 files. This commit fixes this.

--- projects/ngx-extended-pdf-viewer/assets/op-chaining-support.js
+++ projects/ngx-extended-pdf-viewer/assets/op-chaining-support.js
@@ -1,30 +1,33 @@
 // This file provokes syntax errors if your browser doesn't support
 // some new JavaScript standard. This allows ngx-extended-pdf-viewer
 // to switch to the slow but reliable legacy builds.
-const optionalChaining = {
-  support: true,
-};
+if (window.supportsOptionalChaining === undefined) {
+  window.supportsOptionalChaining = false; // set the default value
 
-// Does your browser support the logical assignment operators?
-let x = false;
-x ||= true;
+  const optionalChaining = {
+    support: true,
+  };
 
+  // Does your browser support the logical assignment operators?
+  let x = false;
+  x ||= true;
 
-// Does your browser doesn't support private fields?
-class ClassWithPrivateField {
-  #privateField;
-}
-
-// Does your browser doesn't support private methods?
-class ClassWithPrivateMethod {
-  #privateMethod() {
-    return true;
+  // Does your browser doesn't support private fields?
+  class ClassWithPrivateField {
+    #privateField;
   }
 
-  constructor() {
-    this.#privateMethod();
-  }
-}
-new ClassWithPrivateMethod();
+  // Does your browser doesn't support private methods?
+  class ClassWithPrivateMethod {
+    #privateMethod() {
+      return true;
+    }
 
-window.supportsOptionalChaining = optionalChaining?.support;
+    constructor() {
+      this.#privateMethod();
+    }
+  }
+  new ClassWithPrivateMethod();
+
+  window.supportsOptionalChaining = optionalChaining?.support;
+}
