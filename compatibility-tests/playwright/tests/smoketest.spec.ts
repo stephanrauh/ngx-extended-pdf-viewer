d121b6dd876642ea95ada56d55fd12a6746e2ec0
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2022-06-26 21:19:08 +0200

    restrict test screenshot to a smaller part of the screen

--- compatibility-tests/playwright/tests/smoketest.spec.ts
+++ compatibility-tests/playwright/tests/smoketest.spec.ts
@@ -8,7 +8,6 @@ test.describe('Smoketest', () => {
   test('at least one page renders', async ({ page }) => {
     await page.waitForSelector('.visiblePageIsLoading');
     await expect(page.locator('.visiblePageIsLoading')).toBeVisible();
-    await page.locator('[data-page-number]="19"');
     await page.locator('[data-page-number]="19" > canvas');
     await expect(page.locator('small')).toContainText('Copyright hint: the PDF file has been published by');
     await expect(page.locator('#primaryPageRotateCw')).not.toBeDisabled();
@@ -16,6 +15,7 @@ test.describe('Smoketest', () => {
     await page.waitForSelector(':nth-match(.page canvas,1)');
     await expect(page.locator('.visiblePageIsLoading')).not.toBeVisible();
     await expect(page.locator(':nth-match(.loadingInProgress,1)')).not.toBeVisible();
-    expect(await page.screenshot()).toMatchSnapshot({ maxDiffPixelRatio: 0.001 });
+    expect(await page.locator('pdf-toolbar').screenshot()).toMatchSnapshot({ maxDiffPixelRatio: 0.001 });
+    expect(await page.locator('ngx-extended-pdf-viewer').screenshot()).toMatchSnapshot({ maxDiffPixelRatio: 0.001 });
   });
 });
