8cecb415edf3c3f5c34075fce742b1f1f2a3ceb2
Alexander Pahn <alexander.pahn@outlook.com>
2024-09-29 13:44:09 +0200

    only apply blocking stiles on smaller screens

--- projects/showcase/src/app/core/directives/block-on-open-sidebar.directive.ts
+++ projects/showcase/src/app/core/directives/block-on-open-sidebar.directive.ts
@@ -8,15 +8,15 @@ import { SidebarService } from '../../shared/services/sidebar.service';
 export class BlockOnOpenSidebarDirective {
   private sidebarService = inject(SidebarService);
 
-  @HostBinding('class.pointer-events-none') get pointerEvents() {
+  @HostBinding('class.max-lg:pointer-events-none') get pointerEvents() {
     return this.sidebarService.isOpen();
   }
 
-  @HostBinding('attr.tabindex') get tabIndex() {
+  @HostBinding('attr.max-lg:tabindex') get tabIndex() {
     return this.sidebarService.isOpen() ? -1 : null;
   }
 
-  @HostBinding('attr.aria-hidden') get ariaHidden() {
+  @HostBinding('attr.max-lg:aria-hidden') get ariaHidden() {
     return this.sidebarService.isOpen();
   }
 }
