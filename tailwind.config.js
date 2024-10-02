c65d6ebda49d18507fc33cf6537ff117d2371271
Alexander Pahn <alexander.pahn@outlook.com>
2024-10-02 17:01:48 +0200

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./projects/showcase/src/**/*.{html,ts}'],
   theme: {
     extend: {
       spacing: {
-        'header-height': '6rem',
+        'header-height': 'var(--header-height)',
+      },
+      height: {
+        'vh-wo-header': 'calc(100vh - var(--header-height))',
+      },
+      colors: {
+        primary: {
+          light: 'hsl(0,96%,89%)',
+          variant: {
+            light: 'hsl(329,46%,78%)',
+          },
+        },
+        secondary: {
+          light: 'hsl(193,100%,32%)',
+          variant: {
+            light: 'hsl(183,100%,23%)',
+          },
+        },
+        background: '#ffffff',
+        surface: '#ffffff',
+        error: {
+          light: 'hsl(0,83%,35%)',
+        },
+        on: {
+          primary: {
+            light: 'hsl(0,0%,12%)',
+          },
+          secondary: {
+            light: 'hsl(0,0%,99%)',
+          },
+          background: 'hsl(0,0%,99%)',
+          surface: 'hsl(0,0%,99%)',
+          error: 'hsl(0,0%,99%)',
+        },
       },
     },
   },
