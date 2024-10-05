3c506b5c6848b8bd68eb2586863bdd0d37b4e6ac
Alexander Pahn <alexander.pahn@outlook.com>
2024-10-05 13:17:47 +0200

    extracted routes

--- projects/showcase/src/app/app.routes.ts
+++ projects/showcase/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { ContentPageComponent } from './core/content-page/content-page.component';
-import { Content } from './shared/types/content.types';
+import { aboutRoutes } from './pages/about/about.routes';
 
 export const routes: Routes = [
   {
@@ -8,43 +7,5 @@ export const routes: Routes = [
     pathMatch: 'full',
     redirectTo: 'about',
   },
-  {
-    path: 'about',
-    component: ContentPageComponent,
-    data: {
-      pageTitle: 'About',
-      content: <Content[]>[
-        {
-          type: 'markdown',
-          src: '/assets/pages/about/intro/text.md',
-        },
-      ],
-    },
-  },
-  {
-    path: 'alternatives',
-    component: ContentPageComponent,
-    data: {
-      pageTitle: 'Alternatives',
-      content: <Content[]>[
-        {
-          type: 'markdown',
-          src: '/assets/pages/about/alternatives/text.md',
-        },
-      ],
-    },
-  },
-  {
-    path: 'browser-support',
-    component: ContentPageComponent,
-    data: {
-      pageTitle: 'Browser Support',
-      content: <Content[]>[
-        {
-          type: 'markdown',
-          src: '/assets/pages/about/browser-support/text.md',
-        },
-      ],
-    },
-  },
+  ...aboutRoutes,
 ];
---
+++ projects/showcase/src/app/pages/about/about.routes.ts
@@ -0,0 +1,45 @@
+import { Route } from '@angular/router';
+import { ContentPageComponent } from '../../core/content-page/content-page.component';
+import { Content } from '../../shared/types/content.types';
+
+export const aboutRoutes: Route[] = [
+  {
+    path: 'about',
+    component: ContentPageComponent,
+    data: {
+      pageTitle: 'About',
+      content: <Content[]>[
+        {
+          type: 'markdown',
+          src: '/assets/pages/about/intro/text.md',
+        },
+      ],
+    },
+  },
+  {
+    path: 'alternatives',
+    component: ContentPageComponent,
+    data: {
+      pageTitle: 'Alternatives',
+      content: <Content[]>[
+        {
+          type: 'markdown',
+          src: '/assets/pages/about/alternatives/text.md',
+        },
+      ],
+    },
+  },
+  {
+    path: 'browser-support',
+    component: ContentPageComponent,
+    data: {
+      pageTitle: 'Browser Support',
+      content: <Content[]>[
+        {
+          type: 'markdown',
+          src: '/assets/pages/about/browser-support/text.md',
+        },
+      ],
+    },
+  },
+];
