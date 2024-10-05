c9378d8b94171abdef1833adec2101fbad2e85b1
Alexander Pahn <alexander.pahn@outlook.com>
2024-10-05 16:54:23 +0200

    fixed content page title strategy

--- projects/showcase/src/app/core/content-page/services/content-page-title-strategy.service.ts
+++ projects/showcase/src/app/core/content-page/services/content-page-title-strategy.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
+import { ActivatedRouteSnapshot, PRIMARY_OUTLET, RouterStateSnapshot, TitleStrategy } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
 @Injectable({ providedIn: 'root' })
@@ -11,13 +11,26 @@ export class ContentPageTitleStrategyService extends TitleStrategy {
     super();
   }
   override updateTitle(routerState: RouterStateSnapshot) {
-    const title = this.buildTitle(routerState);
-    const pageTitle = routerState.root.firstChild?.data['pageTitle'];
+    const route = this.getActivatedRoute(routerState);
+    const title = this.getResolvedTitleForRoute(route);
+    const pageTitle = route.data['pageTitle'];
 
     this.setTitle(pageTitle ?? title);
   }
 
   private setTitle(title?: string | null) {
     this.title.setTitle(`${title ?? this.defaultTitle} ${this.documentTitleSuffix}`);
+  }
+
+  private getActivatedRoute(snapshot: RouterStateSnapshot): ActivatedRouteSnapshot {
+    let route: ActivatedRouteSnapshot = snapshot.root;
+    let nextRoute: ActivatedRouteSnapshot | undefined = route;
+    while (nextRoute !== undefined) {
+      nextRoute = route.children.find((child) => child.outlet === PRIMARY_OUTLET);
+      if (nextRoute) {
+        route = nextRoute;
+      }
+    }
+    return route;
   }
 }
