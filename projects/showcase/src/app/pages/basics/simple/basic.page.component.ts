1274d4338153460a1fb14b0cf684343858c7f0a6
Alexander Pahn <alexander.pahn@outlook.com>
2024-10-08 09:27:02 +0200

    added split view component with sticky options

--- projects/showcase/src/app/pages/basics/simple/basic.page.component.ts
+++ projects/showcase/src/app/pages/basics/simple/basic.page.component.ts
@@ -3,28 +3,31 @@ import { ContentPageComponent } from '../../../shared/components/content-page/co
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { MarkdownContentComponent } from '../../../shared/components/markdown-content.component';
 import { SetMinifiedLibraryUsageDirective } from '../../../shared/directives/set-minified-library-usage.directive';
+import { SplitViewComponent } from '../../../shared/components/split-view.component';
 
 @Component({
   selector: 'pvs-basic-page',
   standalone: true,
-  imports: [ContentPageComponent, NgxExtendedPdfViewerModule, MarkdownContentComponent, SetMinifiedLibraryUsageDirective],
+  imports: [ContentPageComponent, NgxExtendedPdfViewerModule, MarkdownContentComponent, SetMinifiedLibraryUsageDirective, SplitViewComponent],
   template: `
     <pvs-content-page [demoTemplate]="demo">
       <pvs-markdown src="/assets/pages/basics/simple/text.md" />
     </pvs-content-page>
 
     <ng-template #demo>
-      <ngx-extended-pdf-viewer
-        slot="viewer"
-        [src]="'./assets/pdfs/The Public Domain - Enclosing the Commons of the Mind.pdf'"
-        [zoom]="'auto'"
-        [height]="'100%'"
-        [textLayer]="true"
-        [showPresentationModeButton]="true"
-        [page]="1"
-        [filenameForDownload]="'The Public Domain - Enclosing the Commons of the Mind.pdf'"
-        pvsSetMinifiedLibraryUsage
-      />
+      <pvs-split-view [stickyEnd]="true">
+        <ngx-extended-pdf-viewer
+          slot="end"
+          [src]="'./assets/pdfs/The Public Domain - Enclosing the Commons of the Mind.pdf'"
+          [zoom]="'auto'"
+          [height]="'100%'"
+          [textLayer]="true"
+          [showPresentationModeButton]="true"
+          [page]="1"
+          [filenameForDownload]="'The Public Domain - Enclosing the Commons of the Mind.pdf'"
+          pvsSetMinifiedLibraryUsage
+        />
+      </pvs-split-view>
     </ng-template>
   `,
 })
---
+++ projects/showcase/src/app/shared/components/split-view.component.ts
@@ -0,0 +1,20 @@
+import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'pvs-split-view',
+  standalone: true,
+  imports: [NgClass],
+  template: ` <div class="grid grid-cols-2 gap-8 h-full">
+    <div [ngClass]="{ 'sticky top-content-begin self-start': stickyStart() }">
+      <ng-content />
+    </div>
+    <div [ngClass]="{ 'sticky top-content-begin self-start': stickyEnd() }">
+      <ng-content select="[slot='end']" />
+    </div>
+  </div>`,
+})
+export class SplitViewComponent {
+  stickyStart = input<boolean>(false);
+  stickyEnd = input<boolean>(false);
+}
