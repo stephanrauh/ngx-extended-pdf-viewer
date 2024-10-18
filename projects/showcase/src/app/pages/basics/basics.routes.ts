f0a1da03aae9573af72077734bd0d85456163c7b
Alexander Pahn <alexander.pahn@outlook.com>
2024-10-18 13:54:43 +0200

    added getting started guide

--- projects/showcase/src/app/pages/basics/basics.routes.ts
+++ projects/showcase/src/app/pages/basics/basics.routes.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/router';
 import { RouteData, RouteGroupData } from '../../shared/types/route-data.types';
 import { BasicPageComponent } from './simple/basic.page.component';
+import { GettingStartedPageComponent } from './getting-started/getting-started-page.component';
 
 export const basicsRoutes: Route[] = [
   {
@@ -10,6 +11,13 @@ export const basicsRoutes: Route[] = [
       name: 'Basics',
     },
     children: [
+      {
+        path: 'getting-started',
+        component: GettingStartedPageComponent,
+        data: <RouteData>{
+          pageTitle: 'Getting Started',
+        },
+      },
       {
         path: 'simple',
         component: BasicPageComponent,
