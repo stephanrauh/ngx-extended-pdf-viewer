3b179d4887151a7e5131385b1b45bca16958a9e7
Julian Schmidt <info@julianb.de>
2024-11-08 21:45:30 +0100

    chore: update test configuration and improve Jest setup

--- compatibility-tests/common/demo/src/app/app.component.spec.ts
+++ compatibility-tests/common/demo/src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng14');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng14 app is running!');
-  });
 });
