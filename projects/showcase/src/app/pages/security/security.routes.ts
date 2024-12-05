b7cbfd4017520df38b257c6a471a61ef6be2b07d
Alexander Pahn <alexander.pahn@outlook.com>
2024-12-05 08:46:10 +0100

    added security page

--- projects/showcase/src/app/pages/security/security.routes.ts
+++ projects/showcase/src/app/pages/security/security.routes.ts
@@ -1,13 +1,22 @@
+import { GeneralPageComponent } from './general/general-page.component';
 import { Route } from '@angular/router';
 import { RouteGroupData } from '../../shared/types/route-data.types';
 
 export const securityRoutes: Route[] = [
   {
-    path: '',
+    path: 'security',
     data: <RouteGroupData>{
       name: 'Security',
       key: 'security',
     },
-    children: [],
+    children: [
+      {
+        path: 'general',
+        component: GeneralPageComponent,
+        data: {
+          pageTitle: 'General',
+        },
+      },
+    ],
   },
 ];
