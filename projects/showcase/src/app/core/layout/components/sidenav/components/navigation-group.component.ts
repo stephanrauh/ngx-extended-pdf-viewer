bb2e04f7cdacc3a4eda07adbbe18ea8a683b02a3
Alexander Pahn <alexander.pahn@outlook.com>
2024-12-16 09:47:30 +0100

    fixed icon not switching correctly for open/closed menu groups

--- projects/showcase/src/app/core/layout/components/sidenav/components/navigation-group.component.ts
+++ projects/showcase/src/app/core/layout/components/sidenav/components/navigation-group.component.ts
@@ -1,17 +1,18 @@
 import { Component, computed, input } from '@angular/core';
 import { isNavigationGroup, isNavigationTarget, NavigationGroup } from '../navigation-config.types';
 import { NavigationTargetComponent } from './navigation-target.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'li[pvs-navigation-group]',
   standalone: true,
-  imports: [NavigationTargetComponent],
+  imports: [NavigationTargetComponent, FormsModule],
   template: `
     <div class="group/item">
-      <input type="checkbox" [id]="groupKey()" checked class="peer hidden" />
+      <input type="checkbox" [id]="groupKey()" [(ngModel)]="isOpen" class="peer hidden" />
       <div class="grid grid-cols-[1fr_minmax(0,auto)] font-semibold">
         <label [for]="groupKey()" class="cursor-pointer">{{ group().displayName }}</label>
-        <span role="presentation" class="after:content-['+'] peer-checked:after:content-['-']"></span>
+        <span [attr.data-open]="isOpen" role="presentation" class="after:content-['+'] data-[open='true']:after:content-['-']"></span>
       </div>
       <ol class="ps-2 hidden peer-checked:block">
         @for (child of group().children; track child) {
@@ -31,6 +32,7 @@ import { NavigationTargetComponent } from './navigation-target.component';
 export class NavigationGroupComponent {
   group = input.required<NavigationGroup>();
   groupKey = computed(() => this.group().key);
+  isOpen = true;
   protected readonly isNavigationGroup = isNavigationGroup;
   protected readonly isNavigationTarget = isNavigationTarget;
 }
