0296cdfe400e8d16ef98ec6adf03227b36d11a7a
Alexander Pahn <alexander.pahn@outlook.com>
2024-12-16 10:07:05 +0100

    improved sidenavs handling of toplevel target

--- projects/showcase/src/app/core/layout/components/sidenav/components/navigation-target.component.ts
+++ projects/showcase/src/app/core/layout/components/sidenav/components/navigation-target.component.ts
@@ -4,15 +4,19 @@ import { Event, NavigationStart, Router, RouterEvent, RouterLink, RouterLinkActi
 import { filter } from 'rxjs/operators';
 import { SidebarService } from '../../../../../shared/services/sidebar.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'li[pvs-navigation-target]',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   template: `
-    <a class="ps-2" [routerLink]="target().link" [routerLinkActive]="['text-primary-light', 'border-s-2', 'border-primary-light']">{{
-      target().displayName
-    }}</a>
+    <a
+      [ngClass]="{ 'ps-2': !isTopLevelTarget(), 'cursor-pointer font-semibold': isTopLevelTarget() }"
+      [routerLink]="target().link"
+      [routerLinkActive]="['text-primary-light', 'border-s-2', 'border-primary-light']"
+      >{{ target().displayName }}</a
+    >
   `,
   host: {
     class: 'pt-1 ',
@@ -22,6 +26,7 @@ export class NavigationTargetComponent {
   private router = inject(Router);
   private sidebarService = inject(SidebarService);
   target = input.required<NavigationTarget>();
+  isTopLevelTarget = input<boolean>(false);
 
   constructor() {
     this.router.events
