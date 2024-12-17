b9fbacb60da4ab3f9708ca0b083ecf7955813aad
Alexander Pahn <alexander.pahn@outlook.com>
2024-12-17 10:50:56 +0100

    yet another correction of markup for better accessibility

--- projects/showcase/src/app/core/layout/components/sidenav/components/navigation-group.component.ts
+++ projects/showcase/src/app/core/layout/components/sidenav/components/navigation-group.component.ts
@@ -13,7 +13,7 @@ import { FormsModule } from '@angular/forms';
       <label [for]="groupKey()" class="cursor-pointer">{{ group().displayName }}</label>
       <span [attr.data-open]="isOpen" role="presentation" class="after:content-['+'] data-[open='true']:after:content-['-']"></span>
     </div>
-    <ol class="ps-2 hidden peer-checked:block">
+    <ul class="ps-2 hidden peer-checked:block">
       @for (child of group().children; track child) {
         @if (isNavigationGroup(child)) {
           <li pvs-navigation-group [group]="child"></li>
@@ -21,7 +21,7 @@ import { FormsModule } from '@angular/forms';
           <li pvs-navigation-target [target]="child"></li>
         }
       }
-    </ol>
+    </ul>
   `,
   host: {
     class: '[&:not(:last-child)]:pb-4',
