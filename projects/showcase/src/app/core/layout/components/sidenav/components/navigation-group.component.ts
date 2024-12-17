48a25515e5cc4ddfb96e328f2581c7a030cf231a
Alexander Pahn <alexander.pahn@outlook.com>
2024-12-17 10:27:01 +0100

    removed unused div

--- projects/showcase/src/app/core/layout/components/sidenav/components/navigation-group.component.ts
+++ projects/showcase/src/app/core/layout/components/sidenav/components/navigation-group.component.ts
@@ -8,22 +8,20 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [NavigationTargetComponent, FormsModule],
   template: `
-    <div class="group/item">
-      <input type="checkbox" [id]="groupKey()" [(ngModel)]="isOpen" class="peer hidden" />
-      <div class="grid grid-cols-[1fr_minmax(0,auto)] font-semibold">
-        <label [for]="groupKey()" class="cursor-pointer">{{ group().displayName }}</label>
-        <span [attr.data-open]="isOpen" role="presentation" class="after:content-['+'] data-[open='true']:after:content-['-']"></span>
-      </div>
-      <ol class="ps-2 hidden peer-checked:block">
-        @for (child of group().children; track child) {
-          @if (isNavigationGroup(child)) {
-            <li pvs-navigation-group [group]="child"></li>
-          } @else if (isNavigationTarget(child)) {
-            <li pvs-navigation-target [target]="child"></li>
-          }
-        }
-      </ol>
+    <input type="checkbox" [id]="groupKey()" [(ngModel)]="isOpen" class="peer hidden" />
+    <div class="grid grid-cols-[1fr_minmax(0,auto)] font-semibold">
+      <label [for]="groupKey()" class="cursor-pointer">{{ group().displayName }}</label>
+      <span [attr.data-open]="isOpen" role="presentation" class="after:content-['+'] data-[open='true']:after:content-['-']"></span>
     </div>
+    <ol class="ps-2 hidden peer-checked:block">
+      @for (child of group().children; track child) {
+        @if (isNavigationGroup(child)) {
+          <li pvs-navigation-group [group]="child"></li>
+        } @else if (isNavigationTarget(child)) {
+          <li pvs-navigation-target [target]="child"></li>
+        }
+      }
+    </ol>
   `,
   host: {
     class: '[&:not(:last-child)]:pb-4',
