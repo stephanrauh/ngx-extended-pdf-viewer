a56c591cad88c4acdd97d9c4f6613b8f9c6075fd
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2024-12-29 14:57:55 +0100

    prepare publishing version 22.1.0

--- projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
+++ projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
@@ -4,7 +4,7 @@ const _isIE11 = typeof window === 'undefined' ? false : !!(<any>globalThis).MSIn
 const isEdge = typeof navigator === 'undefined' || /Edge\/\d./i.test(navigator.userAgent);
 const needsES5 = typeof ReadableStream === 'undefined' || typeof Promise['allSettled'] === 'undefined';
 
-export const pdfjsVersion = '4.7.707';
+export const pdfjsVersion = '4.7.708';
 export const pdfjsBleedingEdgeVersion = '4.9.788';
 export function getVersionSuffix(folder: string): string {
   if (folder?.includes('bleeding-edge')) {
