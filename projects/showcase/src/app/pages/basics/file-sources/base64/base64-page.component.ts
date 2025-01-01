ea49ef9d36fe7a517af7a07b4c15f4a0cfb7a495
Alexander Pahn <alexander.pahn@outlook.com>
2025-01-01 13:12:08 +0100

    added base64 demo

--- projects/showcase/src/app/pages/basics/file-sources/base64/base64-page.component.ts
+++ projects/showcase/src/app/pages/basics/file-sources/base64/base64-page.component.ts
@@ -1,24 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { SplitViewComponent } from '../../../../shared/components/split-view.component';
 import { SetMinifiedLibraryUsageDirective } from '../../../../shared/directives/set-minified-library-usage.directive';
 import { ContentPageComponent } from '../../../../shared/components/content-page/content-page.component';
 import { MarkdownContentComponent } from '../../../../shared/components/markdown-content.component';
+import { ButtonDirective } from '../../../../core/directives/button.directive';
+import { HttpClient } from '@angular/common/http';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { pdfData2 } from './base64.data';
 
 @Component({
   selector: 'pvs-base64-page',
   standalone: true,
-  imports: [ContentPageComponent, MarkdownContentComponent, NgxExtendedPdfViewerModule, SplitViewComponent, SetMinifiedLibraryUsageDirective],
+  imports: [ContentPageComponent, MarkdownContentComponent, NgxExtendedPdfViewerModule, SplitViewComponent, SetMinifiedLibraryUsageDirective, ButtonDirective],
   template: `<pvs-content-page [demoTemplate]="demo">
     <pvs-markdown src="/assets/pages/basics/file-sources/base64/text.md" />
     <pvs-markdown src="/assets/pages/basics/file-sources/shared.md" />
     <ng-template #demo>
       <pvs-split-view>
-        <span>Add Demo Controls here</span>
+        <button pvsButton (click)="switchDocument()">Show other document</button>
 
-        <ngx-extended-pdf-viewer slot="end" src="" zoom="auto" [textLayer]="true" [showPresentationModeButton]="true" pvsSetMinifiedLibraryUsage />
+        <ngx-extended-pdf-viewer
+          slot="end"
+          [base64Src]="base64()"
+          zoom="auto"
+          [textLayer]="true"
+          [showPresentationModeButton]="true"
+          pvsSetMinifiedLibraryUsage
+        />
       </pvs-split-view>
     </ng-template>
   </pvs-content-page>`,
 })
-export class Base64PageComponent {}
+export class Base64PageComponent {
+  private httpClient = inject(HttpClient);
+
+  private initialPdf = toSignal(
+    this.httpClient.get<string>('/assets/pdfs/Bootstrap-vs-Material-Design-vs-Prime-vs-Tailwind.base64.txt', { responseType: 'text' as 'json' }),
+  );
+  private secondPdf: string = pdfData2;
+  private firstPdf = signal(true);
+
+  base64 = computed(() => (this.firstPdf() ? this.initialPdf() : this.secondPdf));
+
+  switchDocument(): void {
+    this.firstPdf.update((firstPdf) => !firstPdf);
+  }
+}
