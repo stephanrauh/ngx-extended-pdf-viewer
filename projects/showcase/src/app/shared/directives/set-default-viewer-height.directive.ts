1ad185ad3654dcb225d4fef17f1716c07e737cbc
Alexander Pahn <alexander.pahn@outlook.com>
2025-01-01 11:20:21 +0100

    set default height of viewer in showcase to 98vh

--- projects/showcase/src/app/shared/directives/set-default-viewer-height.directive.ts
+++ projects/showcase/src/app/shared/directives/set-default-viewer-height.directive.ts
@@ -9,6 +9,6 @@ export class SetDefaultViewerHeightDirective implements OnInit {
   private pdfViewer = inject(NgxExtendedPdfViewerComponent);
 
   ngOnInit() {
-    this.pdfViewer.height = '500px';
+    this.pdfViewer.height = '98vh';
   }
 }
