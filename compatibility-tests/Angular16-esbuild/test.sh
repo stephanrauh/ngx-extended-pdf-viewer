6e065640239870a027b4dfc48348fdb2e1209e44
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-01-04 17:57:11 +0100

    removed outdated test files

--- compatibility-tests/Angular16-esbuild/test.sh
+++
@@ -1,9 +0,0 @@
-cp -r ../common/ .
-docker build --no-cache -t ng16-esbuild .
-rm -r  ./demo/src
-rm -r ./nginx
-container_id=$(docker run --rm -d -p 4200:80 ng16-esbuild)
-cd ../playwright/tests
-npm i
-npx playwright test
-docker stop $container_id
--- compatibility-tests/Angular16/test.sh
+++
@@ -1,9 +0,0 @@
-cp -r ../common/ .
-docker build --no-cache -t ng16 . --no-cache
-rm -r  ./demo/src
-rm -r ./nginx
-container_id=$(docker run --rm -d -p 4200:80 ng16)
-cd ../playwright/tests
-npm i
-npx playwright test
-docker stop $container_id
