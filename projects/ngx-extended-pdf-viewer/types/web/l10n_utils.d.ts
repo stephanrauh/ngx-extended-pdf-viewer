5c5a8caebd2042aa7b6c85a824610826b5797093
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-01-04 20:10:56 +0100

    updated the type definitions

--- projects/ngx-extended-pdf-viewer/types/web/l10n_utils.d.ts
+++
@@ -1,9 +0,0 @@
-export type IL10n = import("./interfaces").IL10n;
-export namespace NullL10n {
-    function getLanguage(): any;
-    function getDirection(): any;
-    function get(ids: any, args: null | undefined, fallback: any): Promise<any>;
-    function translate(element: any): Promise<any>;
-    function pause(): any;
-    function resume(): any;
-}
