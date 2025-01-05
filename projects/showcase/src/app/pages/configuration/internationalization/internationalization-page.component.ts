b815f98afffd80c264fef7da245bca11d0172905
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-01-05 23:29:41 +0100

    showcase: added the demo for the i18n page

--- projects/showcase/src/app/pages/configuration/internationalization/internationalization-page.component.ts
+++ projects/showcase/src/app/pages/configuration/internationalization/internationalization-page.component.ts
@@ -1,13 +1,60 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { ContentPageComponent } from '../../../shared/components/content-page/content-page.component';
 import { MarkdownContentComponent } from '../../../shared/components/markdown-content.component';
+import { SplitViewComponent } from '../../../shared/components/split-view.component';
 
 @Component({
   selector: 'pvs-internationalization-page',
   standalone: true,
-  imports: [ContentPageComponent, MarkdownContentComponent, ],
-  template: `<pvs-content-page>
+  imports: [ContentPageComponent, FormsModule, MarkdownContentComponent, NgxExtendedPdfViewerModule, SplitViewComponent],
+  template: `<pvs-content-page [demoTemplate]="demo">
     <pvs-markdown src="/assets/pages/configuration/internationalization/text.md" />
+    <ng-template #demo>
+      <pvs-split-view>
+        <div class="mb-12">
+          <label for="language">Language</label>
+          <select id="language" name="language" [(ngModel)]="language">
+            <option [value]="undefined">default (usually English)</option>
+            <option [value]="'es-ES'">es-ES (Spanish)</option>
+            <option [value]="'de-DE'">de-DE (German)</option>
+            <option [value]="'fr-FR'">fr-FR (French / custom translations)</option>
+            <option [value]="'pt-PT'">pt-PT (Portuguese)</option>
+            <option [value]="'el'">el (Greek)</option>
+            <option [value]="'nl-BE'">nl-BE (Belgian)</option>
+            <option [value]="'ar'">ar (arab)</option>
+          </select>
+        </div>
+        @if (!hidePdfViewer) {
+          <ngx-extended-pdf-viewer
+            slot="end"
+            src="./assets/pdfs/pdf-sample.pdf"
+            [language]="language"
+            [textLayer]="true"
+            height="75vh"
+            pvsSetMinifiedLibraryUsage
+            pvsSetDefaultZoomLevel
+          />
+        }
+      </pvs-split-view>
+    </ng-template>
   </pvs-content-page>`,
 })
-export class InternationalizationPageComponent {}
+export class InternationalizationPageComponent {
+  public hidePdfViewer = false;
+  private _language: string | undefined = 'nl-BE';
+  public get language(): string | undefined {
+    return this._language;
+  }
+  public set language(value: string | undefined) {
+    if (value === this._language) {
+      return;
+    }
+    this.hidePdfViewer = true;
+    setTimeout(() => {
+      this._language = value;
+      this.hidePdfViewer = false;
+    });
+  }
+}
--- projects/showcase/src/app/pages/configuration/internationalization/text.md
+++ projects/showcase/src/app/pages/configuration/internationalization/text.md
@@ -1,4 +1,5 @@
 ## Using a specific language
+
 `ngx-extended-pdf-viewer` ships with translation for more than 100 languages and dialects. When you copy the `node_modules/ngx-extended-pdf-viewer/assets/locale` folder to the `assets/locale` folder of your application, you can select your language by setting the `language` attribute. If you omit this attribute, the default locale of the browser is used.
 
 **Note**: The `language` attribute is only read when the component is drawn initially. Switching a language at runtime requires hiding the viewer shortly or reloading it.
@@ -6,11 +7,7 @@
 ### HTML
 
 ```html
-<ngx-extended-pdf-viewer
-  [src]="'/assets/pdfs/BootsFaces_Deep_Dive_1.0.pdf'"
-  backgroundColor="#ffffff"
-  [language]="language"
-  textLayer="true">
+<ngx-extended-pdf-viewer [src]="'/assets/pdfs/BootsFaces_Deep_Dive_1.0.pdf'" backgroundColor="#ffffff" [language]="language" textLayer="true">
 </ngx-extended-pdf-viewer>
 ```
 
@@ -34,18 +31,16 @@ export class I18nComponent {
     // to free memory
     setTimeout(() => {
       this.hidePdfViewer = false;
-    }, 1000);
+    }, 100);
   }
 }
 ```
-
 
 ## Custom Translations
 
 To use custom translations, copy the viewer.ftl file you want to modify from the `node_module/ngx-extended-pdf-viewer` folder into your project. Then modify it and add a rule to your `angular.json` ensuring the original file is overwritten each time your compile your application.
 
 **Caveat**: Every once in a while, the team at Mozilla or - less frequently - I add a translation. When you modify the default translation, please check the new changes with each major version of pdf.js.
-
 
 ### angular.json
 
@@ -81,5 +76,3 @@ pdfjs-page-scale-actual = Taille réelle
 pdfjs-page-scale-percent = { $scale } per cent
 ...
 ```
-
-
