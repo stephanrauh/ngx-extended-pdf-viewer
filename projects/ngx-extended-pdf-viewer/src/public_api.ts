cb180f633a93dae782a286ecc42521e830046974
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-02-09 21:01:23 +0100

    stephanrauh/ngx-extended-pdf-viewer#2737 added PdfCspPolicyService to the public API

--- projects/ngx-extended-pdf-viewer/src/public_api.ts
+++ projects/ngx-extended-pdf-viewer/src/public_api.ts
@@ -45,6 +45,7 @@ export * from './lib/options/pdf_page_view';
 export * from './lib/options/rendered-textlayer-highlights';
 export * from './lib/options/text-layer-builder';
 export * from './lib/options/verbosity-level';
+export * from './lib/pdf-csp-policy.service';
 export * from './lib/pdf-dialog/pdf-alt-text-dialog/pdf-alt-text-dialog.component';
 export * from './lib/pdf-dialog/pdf-alt-text-settings-dialog/pdf-alt-text-settings-dialog.component';
 export * from './lib/pdf-dialog/pdf-document-properties-dialog/pdf-document-properties-dialog.component';
