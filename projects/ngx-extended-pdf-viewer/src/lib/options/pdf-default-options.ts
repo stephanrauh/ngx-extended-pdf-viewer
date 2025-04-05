2d52b2eb4e0cd1ab40673151a028b062c1834970
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-04-05 15:39:10 +0200

    stephanrauh/ngx-extended-pdf-viewer#2536 fix compatibility to older browsers

--- projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
+++ projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
@@ -4,8 +4,8 @@ const _isIE11 = typeof window === 'undefined' ? false : !!(<any>globalThis).MSIn
 const isEdge = typeof navigator === 'undefined' || /Edge\/\d./i.test(navigator.userAgent);
 const needsES5 = typeof ReadableStream === 'undefined' || typeof Promise['allSettled'] === 'undefined';
 
-export const pdfjsVersion = '4.10.708';
-export const pdfjsBleedingEdgeVersion = '4.10.708';
+export const pdfjsVersion = '4.10.710';
+export const pdfjsBleedingEdgeVersion = '4.10.709';
 export function getVersionSuffix(folder: string): string {
   if (folder?.includes('bleeding-edge')) {
     return pdfjsBleedingEdgeVersion;
