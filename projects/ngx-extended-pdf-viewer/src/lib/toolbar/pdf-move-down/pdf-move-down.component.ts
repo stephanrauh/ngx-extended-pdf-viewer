53e45bc2876b52877c403316480cc820ea7b18a8
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-07-13 00:06:53 +0200

    use eventbus message to trigger the "move page up / down" events

--- projects/ngx-extended-pdf-viewer/src/lib/toolbar/pdf-move-down/pdf-move-down.component.ts
+++ projects/ngx-extended-pdf-viewer/src/lib/toolbar/pdf-move-down/pdf-move-down.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, effect } from '@angular/core';
 import { ResponsiveVisibility } from '../../responsive-visibility';
+import { IPDFViewerApplication } from '../../options/pdf-viewer-application';
+import { PDFNotificationService } from '../../pdf-notification-service';
 
 @Component({
   selector: 'pdf-move-down',
@@ -9,4 +11,21 @@ import { ResponsiveVisibility } from '../../responsive-visibility';
 export class PdfMoveDownComponent {
   @Input()
   public showMoveDownButton: ResponsiveVisibility = true;
+
+  private PDFViewerApplication: IPDFViewerApplication | undefined;
+
+  constructor(private notificationService: PDFNotificationService) {
+    effect(() => {
+      this.PDFViewerApplication = notificationService.onPDFJSInitSignal();
+    });
+  }
+
+  public movePageDown = () => {
+    if (this.PDFViewerApplication) {
+      const currentPage = this.PDFViewerApplication.page;
+      this.PDFViewerApplication.eventBus.dispatch('movePageDown', {
+        source: { pageNumber: currentPage }
+      });
+    }
+  };
 }
