bb5b6ec615c0dd3042c545f1a1d7f19dc776172d
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2025-10-18 21:15:52 +0200

    bumped the version number to 25.6.0-alpha.5

--- projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
+++ projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
@@ -4,8 +4,8 @@ const _isIE11 = typeof window === 'undefined' ? false : !!(<any>globalThis).MSIn
 const isEdge = typeof navigator === 'undefined' || /Edge\/\d./i.test(navigator.userAgent);
 const needsES5 = typeof ReadableStream === 'undefined' || typeof Promise['allSettled'] === 'undefined';
 
-export const pdfjsVersion = '5.4.911';
-export const pdfjsBleedingEdgeVersion = '5.4.1064';
+export const pdfjsVersion = '5.4.914';
+export const pdfjsBleedingEdgeVersion = '5.4.1068';
 export function getVersionSuffix(folder: string): string {
   if (folder?.includes('bleeding-edge')) {
     return pdfjsBleedingEdgeVersion;
