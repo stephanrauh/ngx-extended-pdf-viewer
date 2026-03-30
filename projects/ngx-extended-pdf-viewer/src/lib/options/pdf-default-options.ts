0066834e0bac0e091818a99b81c557e4355da6bb
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2026-03-31 00:20:08 +0200

    bumped the version number to 26.0.0-rc.25

--- projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
+++ projects/ngx-extended-pdf-viewer/src/lib/options/pdf-default-options.ts
@@ -4,8 +4,8 @@ const _isIE11 = typeof window === 'undefined' ? false : !!(<any>globalThis).MSIn
 const isEdge = typeof navigator === 'undefined' || /Edge\/\d./i.test(navigator.userAgent);
 const needsES5 = typeof ReadableStream === 'undefined' || typeof Promise['allSettled'] === 'undefined';
 
-export const pdfjsVersion = '5.4.1399';
-export const pdfjsBleedingEdgeVersion = '5.4.1399';
+export const pdfjsVersion = '5.4.1402';
+export const pdfjsBleedingEdgeVersion = '5.4.1401';
 export function getVersionSuffix(folder: string): string {
   if (folder?.includes('bleeding-edge')) {
     return pdfjsBleedingEdgeVersion;
--- projects/ngx-extended-pdf-viewer/types/web/ngx-extended-pdf-viewer-version.d.ts
+++ projects/ngx-extended-pdf-viewer/types/web/ngx-extended-pdf-viewer-version.d.ts
@@ -1,1 +1,1 @@
-export const ngxExtendedPdfViewerVersion: "26.0.0-rc.24";
+export const ngxExtendedPdfViewerVersion: "26.0.0-rc.25";
